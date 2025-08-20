524c54bfdaad8eba03b81f0fb39b798ff4e3680b
vanshikaUpadhyay <vanshikaupadhyay561@gmail.com>
2025-08-20 12:34:02 +0530

    Refactor AutoRoulette layout and fix deposit validation
    Updated AutoRoulette page to display the roulette wheel above the board using improved layout structure. Also fixed the deposit validation logic in PaymentMethodDetails to ensure proper error handling when the amount is less than ₹500.

--- frontend/src/pages/AutoRoulette.jsx
+++ frontend/src/pages/AutoRoulette.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Header from "../components/header";
 import RouletteBoard from "./RouletteBoard";
+import RouletteGame from "./roulette-game";
 
 const AutoRoulette = () => {
   return (
-    <>
+    <div className="relative w-full flex flex-col items-">
       <Header />
 
-      <RouletteBoard />
-    </>
+      {/* Wheel on top */}
+      <div className="relative w-full flex justify-center">
+        <div className="absolute  left-0 z-20">
+          <RouletteGame />
+        </div>
+      </div>
+
+      {/* Board below */}
+      <div className="w-full z-10">
+        <RouletteBoard />
+      </div>
+    </div>
   );
 };
 
