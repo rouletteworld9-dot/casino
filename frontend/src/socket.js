90fdcbe53a685a0e0eba9dd732fbde809fd00738
dev-eskills <developmenteskills@gmail.com>
2025-09-10 11:53:13 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "https://casino-6w78.onrender.com"; // your backend URL
+// const URL = "https://casino-6w78.onrender.com"; // eskills
+const URL = "https://casino-pet4.onrender.com"; // Roulette worlds
 // const URL = "http://localhost:8080"; // your backend URL
 export const socket = io(URL, { autoConnect: false });
